3e5d205841197a87afa503b52969dff7e61d25d0
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-14 22:38:56 -0300

    :rocket: Start the repo working with Null values.

---
+++ NullHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+using System;
+
+namespace NullHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            //thisCannotBeNull = null;
+            int? thisCouldBeNull = null;
+
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            if(thisCouldBeNull != null)
+            {
+                //int? length = thisCouldBeNull?.Length;
+            }
+            
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
+        }
+    }
+
+    class Address
+    {
+        public string? Building { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+    }
+}
