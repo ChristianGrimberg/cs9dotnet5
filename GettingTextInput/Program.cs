6b6b4220337e0794688cacac65a7b73b45c38f5b
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-15 23:31:00 -0300

    :+1: Getting Text Input.

---
+++ GettingTextInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettingTextInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your First Name and press ENTER: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Type your Age and press ENTER: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+        }
+    }
+}
