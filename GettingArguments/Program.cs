a307e2c443ddaaf984e820ba2cfbde48d782463b
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-16 08:50:03 -0300

    :+1: Getting arguments practice.

---
+++ GettingArguments/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GettingArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 3)
+            {
+                Console.WriteLine("You must specify two colors and cursor size, e.g.");
+                Console.WriteLine("dotnet run --project GettingArguments/ red yellow 50");
+                return; //stop running program
+            }
+
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse
+            (
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true
+            );
+
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse
+            (
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true
+            );
+
+            try
+            {
+                if(OperatingSystem.IsWindows())
+                {
+                    Console.CursorSize = int.Parse(args[2]);
+                }
+            }
+            catch (PlatformNotSupportedException)
+            {
+                
+                Console.WriteLine("The current platform does not support changing the size of the cursor.");
+            }
+            
+            Console.WriteLine($"There are {args.Length} arguments.");
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
