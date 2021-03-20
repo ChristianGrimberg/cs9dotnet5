2fd11eb8e39c8b2077d62865f6fa0833f48104c6
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-20 20:09:59 -0300

    :rocket: Solve FizzBuzz.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write($"{i.ToString()}");
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    
+                    if (i % 5 == 0)
+                    {
+                        Console.Write("Buzz");
+                    }
+                }
+
+                if (i % 100 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else if (i % 10 == 0)
+                {
+                    Console.Write(",\n");
+                }
+                else
+                {
+                    Console.Write(",");
+                }
+            }
+        }
+    }
+}
