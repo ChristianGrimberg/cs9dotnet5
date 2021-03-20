24ad9170866fc7144d21769cfc46a62425fd8fde
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-20 17:01:17 -0300

    :+1: Practice with specific exceptions.

--- HandlingExceptions/Program.cs
+++ HandlingExceptions/Program.cs
@@ -15,6 +15,14 @@ namespace HandlingExceptions
                 int age = int.Parse(input);
                 Console.WriteLine($"You are {age} years old.");
             }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Your age is a valid number format but it's either too big or small.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you entered is not a valid number format.");
+            }
             catch (Exception ex)
             {
                 Console.WriteLine($"{ex.GetType()} says {ex.Message}");
