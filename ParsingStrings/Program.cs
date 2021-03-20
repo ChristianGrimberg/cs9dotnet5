634d63020ece2d542ccb58c22cba5472357e76ca
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-20 00:05:04 -0300

    :+1: Practice parsing strings to types.

---
+++ ParsingStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ParsingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse("38");
+            DateTime birthday = DateTime.Parse("2 November 1982");
+
+            Console.WriteLine($"I was born {age} years ago.");
+            Console.WriteLine($"My birthday is {birthday}.");
+            Console.WriteLine($"My birthday is {birthday:D}.");
+        }
+    }
+}
