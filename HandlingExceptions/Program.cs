28dc23996fb137c2758c86931700421e2a260e07
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-20 16:53:12 -0300

    :+1: Practice handling exceptions.

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before parsing");
+            Console.Write("What's your age? ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+
+            Console.WriteLine("After parsing");
+        }
+    }
+}
