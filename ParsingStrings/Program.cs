1b08ae4b5a40795ba114a9c70ef211457fb04e74
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-20 16:43:16 -0300

    :+1: Practice with TryParse method.

--- ParsingStrings/Program.cs
+++ ParsingStrings/Program.cs
@@ -12,6 +12,19 @@ namespace ParsingStrings
             Console.WriteLine($"I was born {age} years ago.");
             Console.WriteLine($"My birthday is {birthday}.");
             Console.WriteLine($"My birthday is {birthday:D}.");
+
+            Console.WriteLine("\nHow many eggs are there?");
+            int count;
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out count))
+            {
+                Console.WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                Console.WriteLine("I could not parse the input.");
+            }
         }
     }
 }
