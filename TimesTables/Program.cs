44a5fbb0e694fa9d8df02efd2d68ae7a4436ae50
Christian Grimberg <christian.grimberg@outlook.com>
2021-04-03 23:02:15 -0300

    :monkey_face: Working with functions.

---
+++ TimesTables/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TimesTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunsTimeTable();
+        }
+
+        static void TimesTable(byte number)
+        {
+            Console.WriteLine($"This is the {number} times table:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void RunsTimeTable()
+        {
+            bool isNumber;
+
+            do
+            {
+                Console.Write("Enter a number between 0 and 255: ");
+                isNumber = byte.TryParse(Console.ReadLine(), out byte number);
+
+                if(isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter a valid number!");
+                }
+            } while (!isNumber);
+        }
+    }
+}
