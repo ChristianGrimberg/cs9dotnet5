56a63b35377f8899787411a9912b133c74eaf507
Christian Grimberg <christian.grimberg@outlook.com>
2021-04-03 22:25:27 -0300

    :+1: Practice with anonymous types.

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Anonymous Types
+            var location = new { Country = "Argentina", State = "Buenos Aires"}; // Read-Only Properties
+
+            var person = new
+            {
+                Name = "Christian",
+                LastName = "Grimberg",
+                Location = location
+            };
+
+            var persons = new[]
+            {
+                new { Name = "Natalia", LastName = "Carbonell", Location = location},
+                new { Name = "Alma", LastName = "Grimberg", Location = location},
+                person
+            };
+
+            foreach (var p in persons)
+            {
+                Console.WriteLine($"{p.Name} {p.LastName} lives {location.Country} country in the state of {location.State}.");   
+            }
+            #endregion
+        }
+    }
+}
