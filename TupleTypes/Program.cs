085482861e4d37aff687588e6986dc503c75bb2a
Christian Grimberg <christian.grimberg@outlook.com>
2021-04-03 22:42:49 -0300

    :+1: Practice with Tuple Types.

---
+++ TupleTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TupleTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tuple Types
+            var provider1 = (Name:"Microsoft", Product:"Azure");
+            (string Name, string Product) provider2 = ("Amazon", "AWS");
+
+            Console.WriteLine($"The cloud is {provider1.Name} {provider1.Product}.");
+            Console.WriteLine($"The cloud is {provider2.Name} {provider2.Product}.");
+            #endregion
+        }
+    }
+}
