4acebbbf0653fed6e9ac4d1fbcc7fc9e3c9631fb
Christian Grimberg <christian.grimberg@outlook.com>
2021-04-10 18:37:22 -0300

    :+1: Practice with read only fields and constructors.

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -8,12 +8,32 @@ namespace Packt.Shared
     /// </summary>
     public class Person : Object
     {
-        // Fields
+        // fields
         public string Name;
         public DateTime DateOfBirth;
         public WondersOfTheAncientWorld FavoriteAncientWonder;
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
+        // constants
         public const string Species = "Homo Sapien";
+        // read-only fields
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        // constructors
+        public Person()
+        {
+            // set default values for fields
+            // including read-only fields
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
     }
 }
--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -76,6 +76,27 @@ namespace PeopleApp
             );
 
             Console.WriteLine($"{bob.Name} is a {Person.Species}");
+            Console.WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+            var blankPerson = new Person();
+
+            Console.WriteLine
+            (
+                format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated
+            );
+
+            var gunny = new Person("Gunny", "Mars");
+
+            Console.WriteLine
+            (
+                format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated
+            );
         }
     }
 }
